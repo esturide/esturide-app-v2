0ee7b03320941ded1d300de6776a0ecdbe0cca80
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-03-25 17:37:00 -0600

    Format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
-import '@/styles/App.scss'
+import '@/styles/App.scss';
 
 function App() {
-	return (
-		<>
-			<MapContainer
-					center={{lat: 20.566807292503427, lng: -103.22299991414923}}
-					zoom={11}
-					scrollWheelZoom={true}>
-				<TileLayer
-						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				<Marker position={[20.566807292503427, -103.22299991414923]}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
-			</MapContainer>
-		</>
-	)
+  return (
+    <>
+      <MapContainer
+        center={{ lat: 20.566807292503427, lng: -103.22299991414923 }}
+        zoom={11}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[20.566807292503427, -103.22299991414923]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
 }
 
-export default App
+export default App;
