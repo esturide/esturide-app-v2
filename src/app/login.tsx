acb47a1f0e59befec38ce4763da9d5e15a4dbb98
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-04-12 22:03:10 -0600

    Minor changes

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PresentationLayout from '@layouts/PresentationLayout.tsx';
 import Logo from '@components/resources/Logo.tsx';
 import UserInput from '@components/input/UserInput.tsx';
 import Button from '@components/buttons/Button.tsx';
 import HyperLink from '@components/input/HyperLink.tsx';
+import { isMobileDevice } from '$libs/detectDevice.ts';
 
 const LoginPage: React.FC = () => {
+  const [isDesktopDevice, setIsDesktopDevice] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsDesktopDevice(!isMobileDevice());
+  }, []);
 
   const clickRegister = async () => {
     navigate('/register');
   };
 
+  const returnToHome = async () => {
+    navigate('/');
+  };
+
   return (
     <PresentationLayout title={'Iniciar sesion'} header={<Logo />}>
       <div className="flex flex-col items-center mb-4 mx-2 gap-6">
-        <UserInput label={'Usuario'} placeholder={'A'} />
+        <UserInput label={'Usuario'} />
         <UserInput label={'Contraseña'} type="password" />
       </div>
 
-      <div className="mx-3 my-6">
+      <div className="mx-3 my-6 flex flex-col items-center gap-6">
         <Button label={'Iniciar sesion'} />
+        {isDesktopDevice && (
+          <Button label={'Regresar'} onClick={returnToHome} />
+        )}
       </div>
 
       <div className="flex flex-col items-center">
