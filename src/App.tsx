030d9837fe0f95d75e233b4d6e20676dcc2ea253
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-07-29 19:16:20 -0600

    Rename page and reformat layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import ResponsiveLayout from '@layouts/ResponsiveLayout.tsx';
-
 import '@styles/App.scss';
 
 function App() {
-  const items = [
-    { label: 'Home', href: '/', current: true },
-    { label: 'Login', href: '/login', current: false },
-    {
-      label: 'Street route (Demo)',
-      href: '/street-route',
-      current: false,
-    },
-  ];
-
   return (
     <>
-      <ResponsiveLayout items={items}>
-        <p>Hello world</p>
-      </ResponsiveLayout>
+      <p>Hello world from App</p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { DeviceManagementProvider } from '@/context/DeviceManagment.tsx';
 
-import App from '@/App.tsx';
 import LoginPage from '~/login.tsx';
-import UserRegister from '~/register/user.tsx';
+import UserRegister from '~/register';
 import StreetRouteDemo from '~/demo/street-route.tsx';
 import UserHome from '~/home';
+
+import HomeLayout from '@layouts/route/HomeLayout.tsx';
 
 import '@/index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <UserHome />,
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <UserHome />,
+      },
+    ],
   },
   {
     path: '/login',
@@ -27,10 +34,6 @@ const router = createBrowserRouter([
   {
     path: '/street-route',
     element: <StreetRouteDemo />,
-  },
-  {
-    path: '/home',
-    element: <UserHome />,
   },
 ]);
 
