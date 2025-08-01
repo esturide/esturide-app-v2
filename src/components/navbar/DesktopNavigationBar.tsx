da405351d16a3e193cee0ad67e6f0a27406e479f
SealtielFreak <sealtielfreak@yandex.com>
2025-07-31 20:52:48 -0600

    Change user menu

--- src/components/navbar/DesktopNavigationBar.tsx
+++ src/components/navbar/DesktopNavigationBar.tsx
@@ -10,6 +10,8 @@ import {
 import { ItemType } from '@components/navbar/types.ts';
 import { FaBell, FaUser } from 'react-icons/fa';
 import { LuMenu } from 'react-icons/lu';
+import { useUserManager } from '@/context/UserManager.tsx';
+import { useNavigate } from 'react-router-dom';
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
@@ -20,13 +22,77 @@ type Props = {
 };
 
 const DesktopNavigationBar: React.FC<Props> = ({ items }) => {
+  const navigate = useNavigate();
+  const { isAuthenticated, token } = useUserManager();
+
+  const Notifications = () => {
+    const onCLick = async () => {
+      navigate('/notify');
+    };
+
+    return (
+      <button
+        type="button"
+        className="relative rounded-full p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+        onClick={onCLick}
+      >
+        <span className="absolute -inset-1.5" />
+        <span className="sr-only">View notifications</span>
+        <FaBell color={'white'} />
+      </button>
+    );
+  };
+
+  const MenuUser = () => {
+    return (
+      <Menu as="div" className="relative ml-3">
+        <div>
+          <MenuButton className="relative p-2 flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+            <span className="absolute -inset-1.5" />
+            <span className="sr-only">Open user menu</span>
+            <FaUser color={'white'} />
+          </MenuButton>
+        </div>
+        <MenuItems
+          transition
+          className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+        >
+          <MenuItem>
+            <a
+              href="/profile"
+              className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+            >
+              Your Profile
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="/settings"
+              className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+            >
+              Settings
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="/logout"
+              className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+            >
+              Sign out
+            </a>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
+    );
+  };
+
   return (
     <Disclosure as="nav" className="bg-teal-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <LuMenu />
@@ -62,54 +128,13 @@ const DesktopNavigationBar: React.FC<Props> = ({ items }) => {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-            >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <FaBell color={'white'} />
-            </button>
-
             {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
-              <div>
-                <MenuButton className="relative p-2 flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Open user menu</span>
-                  <FaUser color={'white'} />
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-              >
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Sign out
-                  </a>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
+            {isAuthenticated && (
+              <>
+                <Notifications />
+                <MenuUser />
+              </>
+            )}
           </div>
         </div>
       </div>
@@ -125,7 +150,7 @@ const DesktopNavigationBar: React.FC<Props> = ({ items }) => {
               className={classNames(
                 item.current
                   ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  : 'text-white hover:bg-gray-700 hover:text-white',
                 'block rounded-md px-3 py-2 text-base font-medium',
               )}
             >
