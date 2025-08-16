97e3817ed280141187dcc3403283e37634eee8b8
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-08-16 15:47:19 -0600

    Implement list for select role

--- src/app/user/profile/index.tsx
+++ src/app/user/profile/index.tsx
@@ -2,14 +2,29 @@ import { useUserManager } from '@/context/UserManager.tsx';
 import MainLayout from '@layouts/view/MainLayout.tsx';
 import TouchableOption from '@components/buttons/TouchableOption.tsx';
 import { FaArrowRight } from 'react-icons/fa';
+import ToggleInputList from '@components/input/list/ToggleInputList.tsx';
+import SelectOptions, {
+  StringOption,
+} from '@components/input/selector/SelectOptions.tsx';
 
 function UserProfile() {
   const { logout } = useUserManager();
 
+  const options: StringOption[] = [
+    {
+      id: 0,
+      description: 'Pasajero',
+    },
+    {
+      id: 1,
+      description: 'Condcutor',
+    },
+  ];
+
   return (
     <>
       <MainLayout>
-        <div>
+        <div className={'flex flex-col items-center justify-center'}>
           <TouchableOption label={'Cambiar role'} icon={FaArrowRight} />
           <TouchableOption
             label={'Cerrar sesion'}
@@ -19,6 +34,11 @@ function UserProfile() {
             }}
           />
         </div>
+
+        <SelectOptions
+          options={options}
+          onSelect={async function (index: number) {}}
+        />
       </MainLayout>
     </>
   );
