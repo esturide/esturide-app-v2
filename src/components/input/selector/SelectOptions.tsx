d4bf14a327a6561a2f54d2bb6fd30fb13b6e5d1e
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-08-27 08:23:00 -0600

    Fix style in SelectOptions

--- src/components/input/selector/SelectOptions.tsx
+++ src/components/input/selector/SelectOptions.tsx
@@ -184,7 +184,9 @@ const SelectOptions: React.FC<Props> = ({
 
           <ListboxOptions
             transition
-            className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 shadow-lg ring-black/5 focus:outline-hidden data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
+            className={
+              'absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 shadow-lg ring-black/5 focus:outline-hidden data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm rounded-t-[10px] rounded-b-[10px]'
+            }
           >
             <AllElements />
           </ListboxOptions>
