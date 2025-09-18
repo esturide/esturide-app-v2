ee4c5503e6e3ff2fa7311dcffd4782a8a371a745
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-09-18 13:35:56 -0600

    First implement preview page

--- src/app/user/travels/schedule/preview.tsx
+++ src/app/user/travels/schedule/preview.tsx
@@ -1,28 +1,18 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 import { useNavigate } from 'react-router-dom';
 import TravelMessage from '@components/resources/message/TravelMessage.tsx';
 
-type RouteParams = {
+interface LocationState {
   readonly addressFrom: string;
   readonly addressTo: string;
-};
+}
 
 function PreviewScheduleTravel() {
-  const { addressTo, addressFrom } = useParams<RouteParams>();
+  const { state } = useLocation();
+  const { addressTo, addressFrom } = state as LocationState;
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log(addressTo);
-    console.log(addressFrom);
-  }, [navigate]);
-
-  const onSchedule = async (addressFrom: string, addressTo: string) => {
-    navigate('/home/travels/schedule/config', {
-      state: { addressTo: addressTo, addressFrom: addressFrom },
-    });
-  };
 
   return (
     <div className={'flex flex-col gap-4'}>
