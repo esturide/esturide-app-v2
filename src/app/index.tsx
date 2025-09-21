4b58f3a49c0f749502696ae8a2c0ecad8b853156
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-09-20 19:01:08 -0600

    Random benefits message

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,6 +6,8 @@ import { GrLogin, GrUserNew } from 'react-icons/gr';
 import { useUserManager } from '@/context/UserManager.tsx';
 import { useEffect } from 'react';
 import MainLayout from '@layouts/view/MainLayout.tsx';
+import SimpleCarPresentation from '@components/cards/SimpleCarPresentation.tsx';
+import { shuffleArray } from '$libs/random.ts';
 
 function UserIndex() {
   const { isAuthenticated } = useUserManager();
@@ -43,9 +45,44 @@ function UserIndex() {
     );
   };
 
+  const BenefitsCards = () => {
+    const allBenefits = [
+      <SimpleCarPresentation title={'Conecta. Comparte. Viaja.'}>
+        <p className={'font-light'}>
+          Una plataforma simple para encontrar o publicar viajes en coche. Únete
+          a miles de personas que ya ahorran y contribuyen al medio ambiente, un
+          viaje a la vez. Tu próximo trayecto empieza aquí.
+        </p>
+      </SimpleCarPresentation>,
+
+      <SimpleCarPresentation title={'Tu viaje ideal te espera.'}>
+        <p className={'font-light'}>
+          Encuentra o publica viajes en coche de forma segura y sencilla.
+          Descubre la manera más económica de viajar y conecta con una red de
+          personas que, como tú, buscan un transporte inteligente y amigable con
+          el planeta.
+        </p>
+      </SimpleCarPresentation>,
+
+      <SimpleCarPresentation
+        title={'Únete a la comunidad que transforma cada viaje.'}
+      >
+        <p className={'font-light'}>
+          Comparte trayectos para ahorrar, conectar con personas y construir un
+          futuro más sostenible. Con nosotros, cada kilómetro cuenta para hacer
+          del mundo un lugar mejor.
+        </p>
+      </SimpleCarPresentation>,
+    ];
+
+    return (
+      <div className={'flex flex-col gap-8'}>{shuffleArray(allBenefits)}</div>
+    );
+  };
+
   const WelcomeMessage = () => {
     return (
-      <div className={'flex flex-col items-start h-screen gap-4'}>
+      <div className={'flex flex-col items-start gap-6'}>
         <Welcome dark />
         <UserOptions />
       </div>
@@ -54,7 +91,10 @@ function UserIndex() {
 
   return (
     <MainLayout>
-      <WelcomeMessage />
+      <div className={'flex flex-col gap-8'}>
+        <WelcomeMessage />
+        <BenefitsCards />
+      </div>
     </MainLayout>
   );
 }
---
+++ src/components/cards/SimpleCarPresentation.tsx
@@ -0,0 +1,25 @@
+import HeaderText from '@components/text/HeaderText.tsx';
+
+import '@styles/background/gradient/gradient-animation-clear.scss';
+
+type Props = {
+  title: string;
+};
+
+function SimpleCarPresentation({
+  title,
+  children,
+}: React.PropsWithChildren<Props>) {
+  return (
+    <div
+      className={
+        'bg-gradient-animated-clear flex flex-col items-start gap-2 bg-white rounded-lg px-6 py-4 shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out'
+      }
+    >
+      <HeaderText title={title} weight={1} />
+      {children}
+    </div>
+  );
+}
+
+export default SimpleCarPresentation;
