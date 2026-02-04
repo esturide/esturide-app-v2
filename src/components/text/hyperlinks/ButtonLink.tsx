e0d878b8e903ac26393a56588b92f94d3c3ba152
SealtielFreak <sealtielfreak@yandex.com>
2026-02-03 22:28:54 -0600

    Changes in ButtonLink

--- src/components/text/hyperlinks/ButtonLink.tsx
+++ src/components/text/hyperlinks/ButtonLink.tsx
@@ -13,20 +13,22 @@ function ButtonLink({
   theme = 'teal',
 }: ButtonLinkProps) {
   const buttonStyle = {
-    gray: 'w-full items-center h-full justify-center flex bg-gray-400 inset-shadow-sm/50 inset-shadow-gray-500/50 text-white py-2 px-4 rounded-xl hover:text-gray-100 hover:inset-shadow-sm',
-    teal: 'w-full items-center h-full justify-center flex bg-teal-400 inset-shadow-sm/50 inset-shadow-teal-500/50 text-white py-2 px-4 rounded-xl hover:text-gray-100 hover:inset-shadow-sm',
+    gray: 'w-full h-full items-center justify-center flex bg-gray-400 inset-shadow-sm/50 inset-shadow-gray-500/50 text-white py-2 px-4 rounded-xl hover:text-gray-100 hover:inset-shadow-sm',
+    teal: 'w-full h-full items-center justify-center flex bg-teal-400 inset-shadow-sm/50 inset-shadow-teal-500/50 text-white py-2 px-4 rounded-xl hover:text-gray-100 hover:inset-shadow-sm',
     indigo:
-      'w-full items-center h-full justify-center flex bg-indigo-400 inset-shadow-sm/50 inset-shadow-indigo-500/50 text-white py-2 px-4 rounded-xl hover:text-gray-100 hover:inset-shadow-sm',
+      'w-full h-full items-center justify-center flex bg-indigo-400 inset-shadow-sm/50 inset-shadow-indigo-500/50 text-white py-2 px-4 rounded-xl hover:text-gray-100 hover:inset-shadow-sm',
   };
 
   return (
-    <Link
-      className={buttonStyle[theme]}
-      to={to}
-      target={external ? '_blank' : '_self'}
-    >
-      <p className={'text-center align-middle'}>{label}</p>
-    </Link>
+    <div className={'w-full'}>
+      <Link
+        className={buttonStyle[theme]}
+        to={to}
+        target={external ? '_blank' : '_self'}
+      >
+        <p className={'text-center align-middle'}>{label}</p>
+      </Link>
+    </div>
   );
 }
 
